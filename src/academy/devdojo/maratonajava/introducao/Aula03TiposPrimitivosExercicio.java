c8ccad55a63c6d20e0d22a50ace3803545ca7a80
MichaelKristiansen <michaelbritokristiansen@gmail.com>
2026-01-21 09:33:00 -0300

    docs: aula 03 - Exercício de tipos primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main (String[] args){
+        String nome = "Joao";
+        String endereco = "Rua Juca Tupinamba";
+        double salario =  5431.12;
+        String dataRecebimentoSalario = "20/12/2021";
+        String relatorio = "Eu "+nome+", morando no endereço "+endereco+" confirmo o recebimento do salario "+salario+", na data "+dataRecebimentoSalario;
+        System.out.println(relatorio);
+    }
+}

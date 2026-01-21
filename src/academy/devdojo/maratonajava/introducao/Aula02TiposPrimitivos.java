e9fdc6ed77daefc7a530633c54bf64fa2ca8294b
MichaelKristiansen <michaelbritokristiansen@gmail.com>
2026-01-21 09:20:00 -0300

    docs: aula 02 - Tipos primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    static void main() {
+        // int, double, float, char, byte, short, long, boolean
+        //casting
+        int idade = 10;
+        System.out.println(idade);
+
+        //String
+        String nome = "Goku";
+        System.out.println("oi meu nome é "+nome);
+    }
+}

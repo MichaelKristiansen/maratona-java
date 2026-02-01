b249c22a4d6645d3d6ffec462ae919a382a9ab50
MichaelKristiansen <michaelbritokristiansen@gmail.com>
2026-02-01 11:18:00 -0300

    docs: javacore - Associação

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest03.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.domino.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassociacao.domino.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pelé");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador, jogador2};
+
+        jogador.setTime(time);
+        jogador2.setTime(time);
+
+        time.setJogadores(jogadores);
+
+        jogador.imprime();
+        jogador2.imprime();
+        time.imprime();
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/LeituraDoTecladoTest02.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+public class LeituraDoTecladoTest02 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}

a5685d95089e7923ee05fe683dd4f2ef6d624d74
MichaelKristiansen <michaelbritokristiansen@gmail.com>
2026-02-02 19:50:00 -0300

    docs: javacore - Herança

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-209");
+        Pessoa pessoa = new Pessoa("Izuku Midorya");
+        pessoa.setCpf("000.000.000-00");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario("Oda Nobunaga");
+        funcionario.setCpf("222.222.222.-22");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+        System.out.println("----------");
+        funcionario.imprime();
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+public class HerancaTest02 {
+    //aula75 - sequencia de inicialização
+
+//    0 - bloco de inicialização estatico da super classe é executado quando a JVM carregar a classe pai
+//    1 - bloco de inicialização estatico da subclasse é executado quando a JVM carregar a classe filha
+//    2 - Alocado espaço em memória para o objeto da superclasse
+//    3 - Cada atributo de classe é criado e inicializado com valores default ou o que for passado da
+//    4 - Bloco de inicialização da superclasse é executado na ordem em que aparece
+//    5 - Construtor é executado da superclasse
+//    6 - Alocado espaço em memória para o objeto da subclasse
+//    7 - Cada atributo de subclasse é criado e inicializado com valores default ou o que for passado
+//    8 - Bloco de inicialização da subclasse é executado na ordem em que aparece
+//    9 - Construtor é executado da subclasse
+}

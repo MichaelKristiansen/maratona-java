e78fcbd43ea3164219d930311786c061ed6df3d5
MichaelKristiansen <michaelbritokristiansen@gmail.com>
2026-02-03 18:35:00 -0300

    docs: javacore - Modificador final

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Ferrari.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
+
+public class Ferrari extends Carro {
+    // Aula 79 - modificador "final" garante que um metodo sera usado daquela forma sem ser sobre-escrito ou alterado
+    // serve pra classe tambem
+//    public void imprime(){
+//
+//    }
+}

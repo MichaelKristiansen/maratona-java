a4dc89b45e8a747fe99a0f0689bd9f0f68d2d87e
MichaelKristiansen <michaelbritokristiansen@gmail.com>
2026-02-04 17:16:00 -0300

    docs: javacore - Enum

---
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.Kenum.dominio;
+
+public enum TipoCliente {
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Juridica");
+
+    public final int VALOR;
+    public final String nomeRelatorio;
+
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.VALOR = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public static TipoCliente tipoClientePorNomeRelatorio (String nomeRelatorio){
+        for (TipoCliente tipoCliente : values()) {
+            if(tipoCliente.getNomeRelatorio().equals(nomeRelatorio )){
+                return tipoCliente;
+            }
+        }
+        return null;
+    }
+
+
+
+    public int getVALOR() {
+        return VALOR;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
+}

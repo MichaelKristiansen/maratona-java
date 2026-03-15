7e3428abd5a84c9e9df952a68682322868b85ef4
MichaelKristiansen <michaelbritokristiansen@gmail.com>
2026-03-15 12:17:00 -0300

    docs: aula 05 - Estruturas condicionais (parte 1, revisão)

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais01 {
+    public static void main (String[] args){
+        int idade = 15;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+        // !
+        if(isAutorizadoComprarBebida){
+            System.out.println("Autorizado a comprar bebida alcolica");
+        }else{
+            System.out.println("Nao Autorizado a comprar bebida alcolica!!!!!!!!");
+        }
+
+        if(isAutorizadoComprarBebida == false){
+            System.out.println("Nao Autorizado a comprar bebida alcolica");
+        }
+
+        if(!isAutorizadoComprarBebida){
+            System.out.println("Nao Autorizado a comprar bebida alcolica");
+        }
+        System.out.println("fora do if");
+    }
+}
